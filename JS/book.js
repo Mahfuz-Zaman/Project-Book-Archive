9f0d3557e406472dae00086c52a83ef22c864b0c
Mahfuz-Zaman <zaman.13121996@.com>
2021-09-02 18:53:02 +0600

    error handling

--- JS/book.js
+++ JS/book.js
@@ -1,5 +1,4 @@
 document.getElementById('error-message').style.display = "none";
-
 const searchBook = () => {
     const searchBookInput = document.getElementById('search-field');
     const searchText = searchBookInput.value;
@@ -8,59 +7,53 @@ const searchBook = () => {
     if (searchText == "") {
         displayError()
     }
-
     else {
         document.getElementById('error-message').style.display = "none";
         const url = `https://openlibrary.org/search.json?q=${searchText}`
-
         fetch(url)
             .then(res => res.json())
             .then(data => {
                 displaySearchTotal(data.numFound);
-                displaySearch(data.docs)
+                displaySearch(data.docs);
             }
             );
-
-
+    }
+}
+const displaySearchTotal = item => {
+    if (item === 0) {
+        // alert('something wrong')
+        document.getElementById("message").innerText = `Please Enter a valid Name`
+        document.getElementById("find-message").textContent = "";
+    }
+    else {
+        document.getElementById("find-message").innerText = `Total book Found  ${item}`
+        document.getElementById("message").textContent = "";
     }
 
-
-}
-
-const displaySearchTotal = item => {
-    document.getElementById("find-message").innerText = `Total book Found  ${item}`
 }
 const displaySearch = books => {
     const searchResult = document.getElementById('search-result');
-    if (searchResult == null) {
-        displayError();
-    }
-    else {
-        searchResult.textContent = "";
-        books.forEach(book => {
 
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `            
-            <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : "N/A"}-M.jpg" class="card-img-top bg-white" alt="Images is not Available">
-            <div class="card-body">
-                <h4 class="card-title">Book Title:${book.title}</h4>
-                <h5><small>Author:${book.author_name}</small></h5>
-                <small>First published On:${book.first_publish_year}</small>
-                
-            </div>
-        </div>`;
-            searchResult.appendChild(div);
-        })
-    }
-
+    searchResult.textContent = "";
+    books.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `            
+                <div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : "N/A"}-M.jpg" class="card-img-top bg-white" alt="Images is not Available">
+                <div class="card-body">
+                    <h4 class="card-title">Book Title:${book.title}</h4>
+                    <h5><small>Author:${book.author_name[0] ? book.author_name[0] : book.author_name}</small></h5>
+                    <small>First published On:${book.first_publish_year}</small>          
+                </div>
+            </div>`;
+        searchResult.appendChild(div);
+    })
 }
-
-
 
 const displayError = () => {
     document.getElementById('error-message').style.display = "block";
     document.getElementById('search-result').textContent = "";
     document.getElementById("find-message").innerText = "";
+    document.getElementById("message").textContent = "";
 }
