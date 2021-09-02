0c57f1f728c6a3208a1ee0a4e821b977d35ed4ca
Mahfuz-Zaman <zaman.13121996@.com>
2021-09-02 14:34:54 +0600

    add total

--- JS/book.js
+++ JS/book.js
@@ -13,24 +13,31 @@ const searchBook = () => {
         document.getElementById('error-message').style.display = "none";
         //const proxy = "https://cors-anywhere.herokuapp.com/"
         const url = `http://openlibrary.org/search.json?q=${searchText}`
+
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySearch(data.docs));
+            .then(data => {
+                displaySearch11(data.numFound);
+                displaySearch(data.docs)
+            }
+            );
+
 
     }
 
 
 }
 
+const displaySearch11 = item => {
+    document.getElementById("find-message").innerText = `Total book Found  ${item}`
+}
 const displaySearch = books => {
     const searchResult = document.getElementById('search-result');
-    if (searchResult == undefined) {
+    if (searchResult == null) {
         displayError();
     }
     else {
         searchResult.textContent = "";
-        document.getElementById("find-message").innerText = `Total book Found  ${books.length}`
-
         books.forEach(book => {
 
             const div = document.createElement('div');
@@ -39,9 +46,10 @@ const displaySearch = books => {
             <div class="card h-100">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : "N/A"}-M.jpg" class="card-img-top bg-white" alt="Images is not Available">
             <div class="card-body">
-                <h5 class="card-title">Book Title:${book.title}</h5>
-                <h5 class="card-title">Author:${book.author_name}</h5>
-                <h6 class="card-title"> First published On:${book.first_publish_year}</h6>
+                <h4 class="card-title">Book Title:${book.title}</h4>
+                <h5><small>Author:${book.author_name}</small></h5>
+                <small>First published On:${book.first_publish_year}</small>
+                
             </div>
         </div>`;
             searchResult.appendChild(div);
